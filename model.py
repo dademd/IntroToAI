f2d8b8e1546680bd5b309204db6637a735b43980
danila <d.danko@innopolis.university>
2021-03-29 17:09:23 +0300

    rewrote index calculation with matrices

--- model.py
+++ model.py
@@ -1,90 +1,71 @@
-
 import numpy as np
 
 max_visibility_distance = 7
 default_visibility_distance = 4
 board_size = 6
 
-chebyshev_list = [[] for i in range(max_visibility_distance+1)]
+np.random.seed(1828283)
 
-for d in range(1, max_visibility_distance+1):
-    xs, ys = np.meshgrid([d], np.arange(-d, d+1))
-    chebyshev_list[d] = np.array([xs.ravel(), ys.ravel()])
+# chebyshev_line = [[] for i in range(max_visibility_distance + 1)]
+
+def get_chebyshev_line(distance):
+    return
 
 
-# directions 0>,1^,2<,3v
+chebyshev_line = [get_chebyshev_line(distance) for distance in range(max_visibility_distance + 1)]
 
-# for each direction, contains multipliers for xs, ys
-# to watch in counter - clockwise direction around map
-# x, y
+clockwise_turn = np.array([[0, -1], [1, 0]])
+clockwise_powers = np.array([np.linalg.matrix_power(clockwise_turn, i) for i in range(4)])
 
-# counter-clockwise codes for indices
-# ys in clockwise go from lower to upper values
-#   -> anti-clockwise is -ys
-cc = np.array([[[1, 0], [1, 1]],
-               [[0, 1], [1, -1]],
-               [[1, 0], [-1, -1]],
-               [[0, 1], [-1, 1]]])
-len_cc = len(cc)
+directions = 4
+
 
 def get_chebyshev_indices(direction, distance):
-    assert 0 <= direction <= 3
-    assert 0 <= distance <= max_visibility_distance
-    if distance == 0:
-        return np.empty(0,int)
-    indices = np.empty((0,2), int)
-    for i in range(direction-1, (direction+1)+1):
-        ind_cc = (len_cc + i) % len_cc
-        perm, mult = cc[ind_cc]
-        chebyshev = chebyshev_list[distance]
-        perm = mult[:, np.newaxis] * np.array([chebyshev[i] for i in perm])
-        indices = np.append(indices, perm.T, axis=0)
-    indices = indices.astype(int)
-    index = np.unique(indices.astype(int), axis=0, return_index=True)[1]
-    return np.array([indices])
+    xs, ys = np.meshgrid([distance], np.arange(-distance, distance + 1))
+    line = np.array([xs.ravel(), ys.ravel()])
+    turn_indices = [(directions + i) % directions for i in range(direction - 1, (direction + 1) + 1)]
+    turned_lines = [np.dot(clockwise_powers[i], line) for i in turn_indices]
+    return np.concatenate((turned_lines), axis=1)
 
-chebyshev_indices = [[get_chebyshev_indices(distance=distance, direction=direction)
-                      for distance in range(0, max_visibility_distance)]
-                     for direction in range(0, 4)]
 
-lst = chebyshev_indices[0][1]
-print(lst)
-# lst_c = np.zeros(lst.shape)
-# for i in range(len(lst)-1):
-#     print(lst[i+1]-lst[i])
-#
-# print(lst_c)
+chebyshev_indices = [[get_chebyshev_indices(direction=direction, distance=distance)
+                      for distance in range(max_visibility_distance + 1)]
+                     for direction in range(4)]
 
-def get_visible_indices(x, y, distance, direction):
-    ind = chebyshev_indices[direction][distance] + np.array([x,y])
+print(chebyshev_indices[0][1])
+
+
+def get_visible_indices(x, y, direction, distance):
+    ind = chebyshev_indices[direction][distance] + np.array([x, y])
     avail_ind = ind[np.all((ind >= 0) & (ind < board_size))]
     return avail_ind
 
-field = np.array([[set() for i in range(board_size)] for j in range(board_size)])
 
-np.random.seed(192)
-id = 2
-xs, ys = np.random.random_integers(0, board_size-1, (2, 10))
-for i, j in zip(xs, ys):
-    field[i][j].add(id)
+field = np.full((board_size, board_size), set())
 
-# print(field)
-
-def obstacle_at_dist(x, y, direction, id):
-    for distance in range(1, max_visibility_distance+1):
-        avail_ind = get_visible_indices(x=x, y=y, distance=distance, direction=direction)
-        if len(chebyshev_indices[direction][distance]) != avail_ind.shape[0]:
-            return distance
-        for i, j in avail_ind:
-            if id in field[i][j]:
-                return distance
-    return default_visibility_distance
-
-snakes_number = 20
-directions = 7
-# 0-cells from directions store snake's scores+
-
-# snakes_eyes = np.random.rand((snakes_number, directions, max_visibility_distance+1))
-
-# get_visible_indices(x=2, y=4, distance=2, direction=2)
-obstacle_at_dist(x=3, y=3, direction=0, id = 2)
+# np.random.seed(192)
+# id = 2
+# xs, ys = np.random.random_integers(0, board_size-1, (2, 10))
+# for i, j in zip(xs, ys):
+#     field[i][j].add(id)
+#
+# # print(field)
+#
+# def obstacle_at_dist(x, y, direction, id):
+#     for distance in range(1, max_visibility_distance+1):
+#         avail_ind = get_visible_indices(x=x, y=y, distance=distance, direction=direction)
+#         if len(chebyshev_indices[direction][distance]) != avail_ind.shape[0]:
+#             return distance
+#         for i, j in avail_ind:
+#             if id in field[i][j]:
+#                 return distance
+#     return default_visibility_distance
+#
+# snakes_number = 20
+# directions = 7
+# # 0-cells from directions store snake's scores+
+#
+# # snakes_eyes = np.random.rand((snakes_number, directions, max_visibility_distance+1))
+#
+# # get_visible_indices(x=2, y=4, distance=2, direction=2)
+# obstacle_at_dist(x=3, y=3, direction=0, id = 2)
